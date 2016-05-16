8e650d5990d004af12c2a7984251d5fa0cb53ea0
voidnullvoidnull <dimaek@li.ru>
2016-05-16 13:26:44 +0400

    Snike start commit

--- Assets/Scripts/ObstackleGenerator.cs
+++ Assets/Scripts/ObstackleGenerator.cs
@@ -1,8 +1,9 @@
 ﻿using UnityEngine;
 
-public class ObstackleGenerator : MonoBehaviour, IGenerator {
+public class ObstackleGenerator : MonoBehaviour, IGenerator
+{
 
-	public GameObject obsacklePrefab;
+    public GameObject obsacklePrefab;
     public Transform rightTop;
     public Transform leftBottom;
     public int interval = 1;
@@ -20,34 +21,34 @@ public class ObstackleGenerator : MonoBehaviour, IGenerator {
     {
         cameraRightTop = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height));
         rightTop.position = new Vector3(cameraRightTop.x, cameraRightTop.y);
-        
+
         cameraLeftBottom = Camera.main.ScreenToWorldPoint(Vector3.zero);
         leftBottom.position = new Vector3(cameraLeftBottom.x, cameraLeftBottom.y);
-        
-       
+
+
     }
 
     Vector3 GetRandomCoordinates()
     {
         randomSeed += System.DateTime.Now.Second;
         System.Random rnd = new System.Random(randomSeed);
-        
+
         int x, y;
 
         x = rnd.Next((int)leftBottom.position.x + boardersMargin, (int)rightTop.position.x);
         y = rnd.Next((int)leftBottom.position.y + boardersMargin, (int)rightTop.position.y);
 
         while (Mathf.Abs((float)x % 2) != 0)
-            {
-                x = rnd.Next((int)leftBottom.position.x + boardersMargin, (int)rightTop.position.x);
-                randomSeed++;
-            }
+        {
+            x = rnd.Next((int)leftBottom.position.x + boardersMargin, (int)rightTop.position.x);
+            randomSeed++;
+        }
 
-            while (Mathf.Abs((float)y % 2) != 0)
-            {
-                y = rnd.Next((int)leftBottom.position.y + boardersMargin, (int)rightTop.position.y);
-                randomSeed++;
-            }
+        while (Mathf.Abs((float)y % 2) != 0)
+        {
+            y = rnd.Next((int)leftBottom.position.y + boardersMargin, (int)rightTop.position.y);
+            randomSeed++;
+        }
 
         Vector3 coord = new Vector3(x, y);
         return coord;
@@ -70,12 +71,12 @@ public class ObstackleGenerator : MonoBehaviour, IGenerator {
             int Size = Random.Range(0, randomSize);
             for (int j = 0; j < Size; j++)
             {
-                Vector3 childCoord = coord + new Vector3(j * interval,0);
-                            if(childCoord.x<rightTop.position.x-2)
-                                Instantiate(obsacklePrefab, childCoord, Quaternion.identity);
-                        childCoord = coord + new Vector3(-j * interval,0);
-                            if (childCoord.x > leftBottom.position.x+2)
-                                Instantiate(obsacklePrefab, childCoord, Quaternion.identity);
+                Vector3 childCoord = coord + new Vector3(j * interval, 0);
+                if (childCoord.x < rightTop.position.x - 2)
+                    Instantiate(obsacklePrefab, childCoord, Quaternion.identity);
+                childCoord = coord + new Vector3(-j * interval, 0);
+                if (childCoord.x > leftBottom.position.x + 2)
+                    Instantiate(obsacklePrefab, childCoord, Quaternion.identity);
             }
         }
         Debug.Log(System.DateTime.Now.Millisecond - timer + " миллисекунд на генерацию");
